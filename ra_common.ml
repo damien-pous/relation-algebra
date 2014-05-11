6a5a3ff0ebeec082913f701207f62a1d623900a7
ppedrot <ppedrot@0cf17b13-060f-0410-b1b1-c666bec9822a>
2014-05-11 01:56:25 +0000

    Partial fix for RelationAlgebra.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@2617 0cf17b13-060f-0410-b1b1-c666bec9822a

--- ra_common.ml
+++ ra_common.ml
@@ -60,7 +60,7 @@ let fresh_name n goal =
 
 (* access to Coq constants *)
 let get_const dir s = 
-  lazy (Globnames.constr_of_global (Coqlib.find_reference "RelationAlgebra.reification" dir s))
+  lazy (Universes.constr_of_global (Coqlib.find_reference "RelationAlgebra.reification" dir s))
 
 (* make an application using a lazy value *)
 let force_app f = fun x -> mkApp (Lazy.force f,x)
