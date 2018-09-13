1092aff3fb20df12386cffa2d06ad8535b5e79f2
Christian Doczkal <christian.doczkal@ens-lyon.fr>
2018-09-13 16:36:35 +0200

    add all.v exporting everything except examles

---
+++ all.v
@@ -0,0 +1,56 @@
+(** Import everything except the examples *)
+
+Require Export common.
+Require Export comparisons.
+Require Export positives.
+Require Export ordinal.
+Require Export denum.
+Require Export pair.
+Require Export powerfix.
+
+Require Export level.
+Require Export lattice.
+Require Export monoid.
+Require Export kleene.
+Require Export factors.
+Require Export kat.
+
+Require Export rewriting.
+Require Export move.
+
+Require Export lsyntax.
+Require Export syntax.
+Require Export normalisation.
+
+Require Export prop.
+Require Export boolean.
+Require Export rel.
+Require Export lang.
+Require Export lset.
+
+Require Export sups.
+Require Export sums.
+Require Export matrix.
+Require Export matrix_ext.
+
+Require Export untyping.
+
+Require Export regex.
+Require Export rmx.
+Require Export bmx.
+Require Export dfa.
+Require Export nfa.
+Require Export ka_completeness.
+
+Require Export atoms.
+Require Export traces.
+Require Export glang.
+Require Export gregex.
+Require Export kat_completeness.
+Require Export ugregex.
+Require Export ugregex_dec.
+Require Export kat_untyping.
+Require Export kat_reification.
+Require Export kat_tac.
+
+Require Export relalg.
