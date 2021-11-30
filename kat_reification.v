742d62924aa12b3f6f34203648728722f4ae976f
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-11-30 16:09:05 +0100

    adapt to coq/coq#15220

--- kat_reification.v
+++ kat_reification.v
@@ -240,5 +240,4 @@ Arguments p_var _%positive.
 
 (** Load ML reification modules *)
 
-Declare ML Module "ra_common". 
-Declare ML Module "kat_reification". 
+Declare ML Module "coq-relation-algebra.kat_reification". 
